de96e082b85533661b3002d703e8f274702a5de3
Smadey Zhang <365649304@qq.com>
2014-12-21 23:39:33 +0800

    Refine part 5 animation

--- js/bob.js
+++ js/bob.js
@@ -79,12 +79,16 @@ $(document).ready(function() {
         }
     })]);
 
-    $('#part3').length && new ScrollScene({
-        triggerElement: '#part3',
-        triggerHook: 'onLeave',
-        duration: 1200,
-        offset: 170
-    }).setTween(pinAnim).setPin('#part3').addTo(controller);
+    if($('#part3').length) {
+        new ScrollScene({
+            triggerElement: '#part3',
+            triggerHook: 'onLeave',
+            duration: 1200,
+            offset: 170
+        }).setTween(pinAnim).setPin('#part3').addTo(controller);
+
+        $('#part3').parent().css({ 'background-color' : $('#part3').css('background-color') });
+    }
 
 
     var getPointOuterStyle = function(r) {
@@ -104,8 +108,47 @@ $(document).ready(function() {
             .add([TweenMax.to(selector, 0.4, getPointOuterStyle(23))]);
     };
 
-    var stepAnimation = TweenMax.to('#part5', 1, {
-        onStart: function() {
+
+    if($('#part5').length) {
+        var _top = $('#part5').offset().top;
+        var _height = $('#part5').height();
+        var _offset = 0;
+
+        var $root = $(window);
+        var isPlayed = false;
+
+        $root.on('scroll', function() {
+            var _offsetTop = $root.scrollTop();
+
+            if(_offsetTop < _top - _offset) {
+                if(isPlayed) {
+                    isPlayed = false;
+                    reset();
+                }
+            }
+            else if(_offsetTop >= _top - _offset && _offsetTop <= _top + _height) {
+                if(!isPlayed) {
+                    isPlayed = true;
+                    play();
+                }
+            }
+            else if(_offsetTop > _top + _height) {
+                if(isPlayed) {
+                    isPlayed = false;
+                    reset();
+                }
+            }
+
+        });
+
+
+        function reset() {
+            $('.step').css({ width: '', opacity: '' });
+            $('.point').css({ display: '' });
+            $('.point-outer').css({ width: '', height: '', left: '', top: '' });
+        }
+
+        function play() {
             (new TimelineMax).add([TweenMax.to('.step-1', 0.3, {
                 width: '200px',
                 onStart: function() {
@@ -145,12 +188,63 @@ $(document).ready(function() {
                 }
             })]);
         }
-    });
+    }
 
-    $('#part5').length && new ScrollScene({
-        triggerElement: '#part5',
-        triggerHook: 'onLeave',
-        duration: 0.001,
-        offset: -100
-    }).setTween(stepAnimation).setPin('#part5').addTo(controller);
+    // if($('#part5').length) {
+    //     new ScrollScene({
+    //         triggerElement: '#part5',
+    //         triggerHook: 'onLeave',
+    //         duration: 1,
+    //         offset: -300
+    //     }).on('start', function() {
+    //         $('.step').css({ width: '', opacity: '' });
+    //         $('.point').css({ display: '' });
+    //         $('.point-outer').css({ width: '', height: '', left: '', top: '' });
+
+    //         (new TimelineMax).add([TweenMax.to('.step-1', 0.3, {
+    //             width: '200px',
+    //             onStart: function() {
+    //                 $('.step-1').css({ opacity: 1 });
+    //             }
+    //         })]).add([TweenMax.to('.step-2', 0.3, {
+    //             width: '200px',
+    //             onStart: function() {
+    //                 $('.point-1').fadeIn(1000);
+    //                 $('.step-2').css({ opacity: 1 });
+
+    //                 loadPointOuterAnimation('.point-1 > .point-outer');
+    //             }
+    //         })]).add([TweenMax.to('.step-3', 0.3, {
+    //             width: '300px',
+    //             onStart: function() {
+    //                 $('.point-2').fadeIn(1000);
+    //                 $('.step-3').css({ opacity: 1 });
+
+    //                 loadPointOuterAnimation('.point-2 > .point-outer');
+    //             }
+    //         })]).add([TweenMax.to('.step-4', 0.3, {
+    //             width: '180px',
+    //             onStart: function() {
+    //                 $('.point-3').fadeIn(1000);
+    //                 $('.step-4').css({ opacity: 1 });
+
+    //                 loadPointOuterAnimation('.point-3 > .point-outer');
+    //             }
+    //         })]).add([TweenMax.to('.step-5', 0.3, {
+    //             width: '250px',
+    //             onStart: function() {
+    //                 $('.point-4').fadeIn(1000);
+    //                 $('.step-5').css({ opacity: 1 });
+
+    //                 loadPointOuterAnimation('.point-4 > .point-outer');
+    //             }
+    //         })]);
+    //     }).on('leave', function() {
+    //         // $('.step').css({ width: '', opacity: '' });
+    //         // $('.point').css({ display: '' });
+    //         // $('.point-outer').css({ width: '', height: '', left: '', top: '' });
+    //     }).setPin('#part5').addTo(controller);
+
+    //     $('#part5').parent().css({ 'background-color' : $('#part5').css('background-color') });
+    // }
 });
